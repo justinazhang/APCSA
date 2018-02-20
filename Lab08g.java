ec7eac907af8b8cb216d91debaf612433b95769d
zhangj0585 <zhangjustin77@gmail.com>
2018-02-20 08:05:03 -0800

    Unit6

---
+++ Lab08g.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab08g
+{
+	public static void main ( String[] args )
+	{
+		LoopStats chicken = new LoopStats();
+		chicken.setNums(5, 15);
+		out.println(chicken);
+		out.println("total "+chicken.getTotal());
+		out.println("even count " +chicken.getEvenCount());
+		out.println("odd count "+chicken.getOddCount());
+		
+		
+		
+		
+		
+					
+	}
+}
---
+++ Lab09f.java
@@ -0,0 +1,24 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab09f
+{
+	public static void main( String args[] )
+	{
+		LetterRemover chicken = new LetterRemover("Hello",'o');
+		chicken.setRemover("asoasdfoodf",'o');
+		out.println(chicken);
+		
+		
+		
+		
+		
+		
+											
+	}
+}
