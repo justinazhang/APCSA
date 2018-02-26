53a720eedb15a3fa1d691fe9ee156df2177673f6
zhangj0585 <zhangjustin77@gmail.com>
2018-02-26 09:31:15 -0800

    Unit8

---
+++ Unit 8/Lab14d.java
@@ -0,0 +1,21 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab14d
+{
+	public static void main( String args[] )
+	{
+		Grades student =new Grades();
+		student.setGrade(3,new Double[]{6.0,9.0,12.0});
+		System.out.print(student);
+		student.setGrade(6, new Double[]{7.8,9.0,12.7,90.0,50.0,68.0});
+		System.out.print(student);
+		
+		
+		
+		
+	}
+}
